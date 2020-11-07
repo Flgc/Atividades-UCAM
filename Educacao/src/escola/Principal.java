8fef1459df55faa4fe935c2db54dfb23bb83db93
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-07 19:20:16 -0300

    Utilizando javax.swing.JOptionPane - Exemplos

--- Educacao/src/escola/Principal.java
+++ Educacao/src/escola/Principal.java
@@ -8,6 +8,17 @@ public class Principal {
 		
 		Aluno aluno = new AlunoRegular("Carlo", 20, 1, "Prof. Marcos");
 		JOptionPane.showInternalMessageDialog(null, aluno.listarDados());
+		JOptionPane.showInputDialog(aluno.listarDados());
+		JOptionPane.showMessageDialog(null, aluno.listarDados());
+		
+		String valor = JOptionPane.showInputDialog(null, "Informe um número: ");
+		StringBuilder strBuilder = new StringBuilder();
+		
+		for (int i = 1; i <= 10; i++) {
+			strBuilder.append(valor + " X " + i + " = " + Integer.parseInt(valor) * i +  "\n");
+		}
+		
+		JOptionPane.showMessageDialog(null, strBuilder);
 	}
 
 }
