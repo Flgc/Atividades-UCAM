190e1609b77e22fd639f602fd71bda146b3821d0
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-02 21:26:35 -0200

    Classe Arrays

---
+++ ClasseArrays/src/com/ead/programs/TesteArrays.java
@@ -0,0 +1,22 @@
+package com.ead.programs;
+
+import java.util.Arrays;
+
+public class TesteArrays {
+	public static void main(String[] args) {
+		int[]numeros = {1, 4, 0, -13, 2112, 14, 17};
+		int posicao;
+		
+		System.out.println("Os elementos do array são: " + Arrays.toString(numeros));
+		System.out.println("Ordenando...");
+		
+		Arrays.sort(numeros);
+		
+		
+		System.out.println("Array ordenado: " + Arrays.toString(numeros));
+		posicao  = Arrays.binarySearch(numeros, 2112);
+		
+		System.out.println("Posição do elemento '2112': " + posicao );
+	}
+
+}
