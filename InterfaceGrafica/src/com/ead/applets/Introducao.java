ac54fa3458ddd41e3edf2cf47ef325f1f1a44ca2
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-03 14:15:15 -0200

    MultThreading Component GUI - Graphic Interface - Applet

---
+++ InterfaceGrafica/src/com/ead/applets/Introducao.java
@@ -0,0 +1,11 @@
+package com.ead.applets;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Introducao extends Applet{
+	public void paint(Graphics g) {
+		g.drawString("Exemplo de Applet", 40, 50);
+	}
+
+}
