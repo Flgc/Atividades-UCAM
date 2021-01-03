1f40781a2e5336bd89cd14b606ceeddbbc4ebaf1
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-03 18:45:07 -0200

    File Imput/Output - File writed with break of line

--- Arquivos/src/com/ead/arquivos/GravarArquivo.java
+++ Arquivos/src/com/ead/arquivos/GravarArquivo.java
@@ -7,11 +7,12 @@ import javax.swing.JOptionPane;
 public class GravarArquivo {
 	public static void main(String[] args) {
 		try {
-			FileWriter fw = new FileWriter("D:/saida.txt");
+			FileWriter fw = new FileWriter("D:/saida.txt", true);
 			String nome = JOptionPane.showInputDialog("Informe um nome: ");
-			fw.write(nome); 
+			fw.write(nome + "\r\n"); 
 			fw.close();
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
