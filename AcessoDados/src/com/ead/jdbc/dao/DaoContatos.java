eff30ee12b5e896c068bfc155f2a3387a2ece024
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-04 18:28:28 -0200

    Database Access with JDBC - Dao - application developing schedule - CRUD

--- AcessoDados/src/com/ead/jdbc/dao/DaoContatos.java
+++ AcessoDados/src/com/ead/jdbc/dao/DaoContatos.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ead.jdbc.entidade.Contatos;
 
 public class DaoContatos {
 
@@ -21,8 +25,107 @@ public class DaoContatos {
 			throw e;
 		}
 	}
-	
-	private void fechaConexao() throws Exception {
+
+	private void fecharConexao() throws Exception {
 		cn.close();
+	}
+
+	public void incluir(Contatos contato) throws Exception {
+		try {
+			abrirConexao();
+			stmt = cn.prepareStatement("INSERT INTO CONTATOS (NOME_CONTATO, TELEFONE_CONTATO, "
+					+ "EMAIL_CONTATO, DATA_CADASTRO) VALUES (?,?,?,?)");
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getTelefone());
+			stmt.setString(3, contato.getEmail());
+			stmt.setDate(4, new java.sql.Date(contato.getData().getTime()));
+
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			fecharConexao();
+		}
+	}
+
+	public void remover(int id) throws Exception {
+		try {
+			abrirConexao();
+			stmt = cn.prepareStatement("DELETE FROM CONTATOS WHERE ID=?");
+			stmt.setInt(1, id);
+
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			fecharConexao();
+		}
+	}
+
+	public Contatos buscar(int id) throws Exception {
+		Contatos contato = null;
+		try {
+			abrirConexao();
+			stmt = cn.prepareStatement("SELECT * FROM CONTATOS WHERE ID=?");
+			stmt.setInt(1, id);
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				contato = new Contatos();
+				contato.setId(id);
+				contato.setNome(rs.getString("NOME_CONTATO"));
+				contato.setTelefone(rs.getString("TELEFONE_CONTATO"));
+				contato.setEmail(rs.getString("EMAIL_CONTATO"));
+				contato.setData(rs.getDate("DATA_CADASTRO"));
+			}
+		} catch (Exception e){
+			throw e;
+		}finally {
+			fecharConexao();
+		}
+		return contato;
+	}
+	
+	public void alterar(Contatos contato) throws Exception{
+		try {
+			abrirConexao();
+			stmt = cn.prepareStatement("UPDATE CONTATOS SET NOME_CONTATO=?,  TELEFONE_CONTATO=?," +
+															"EMAIL_CONTATO=?,DATA_CADASTRO=? WHERE ID=?");
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getTelefone());
+			stmt.setString(3,contato.getEmail());
+			stmt.setDate(4, new java.sql.Date(contato.getData().getTime()));
+			stmt.setInt(5, contato.getId());
+			
+			stmt.executeUpdate();
+		} catch (Exception e){
+			throw e;
+		}finally {
+			fecharConexao();
+		}
+	}
+
+	public List<Contatos> listar() throws Exception{
+		List<Contatos> contatos = new ArrayList<Contatos>();
+		try {
+			abrirConexao();
+			stmt = cn.prepareStatement("SELECT * FROM CONTATOS");
+			
+			while(rs.next()) {
+				Contatos contato = new Contatos();
+				contato.setId(rs.getInt("ID"));
+				contato.setNome(rs.getString("NOME_CONTATO"));
+				contato.setTelefone(rs.getString("TELEFONE_CONTATO"));
+				contato.setEmail(rs.getString("EMAIL_CONTATO"));
+				contato.setData(rs.getDate("DATA_CADASTRO"));
+				
+				contatos.add(contato);
+			}
+		} catch (Exception e){
+			throw e;
+		}finally {
+			fecharConexao();
+		}
+		return contatos;
 	}
 }
