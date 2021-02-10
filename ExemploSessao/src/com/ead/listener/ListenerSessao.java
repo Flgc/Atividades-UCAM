f96c6a473b533c1cec5d7e92bd59b1bb83c1190a
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-10 00:03:44 -0300

    Java EE - Servlet 3.0 - Java Server Pages (JSP) - iniciando Listeners do formulario - Sessao (Login e senha) - JSP File

---
+++ ExemploSessao/src/com/ead/listener/ListenerSessao.java
@@ -0,0 +1,42 @@
+package com.ead.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+/**
+ * Application Lifecycle Listener implementation class ListenerSessao
+ *
+ */
+@WebListener
+public class ListenerSessao implements HttpSessionAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ListenerSessao() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
