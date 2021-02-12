2364ceb61262b20994f845bb56074e381d58f246
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-11 23:16:57 -0300

    Java EE - Servlet 3.0 - Java Server Pages (JSP) - JSTL - iniciando projeto de exemplo - Ajustes  iniciais inclusao da jstl-1.2.JAR

--- ExemploJSTL/src/com/ead/javabeans/Automovel.java
+++ ExemploJSTL/src/com/ead/javabeans/Automovel.java
@@ -1,34 +1,60 @@
 package com.ead.javabeans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Automovel {
 
 	private String marca, modelo;
 	private int ano;
-	
+
+	public Automovel(String marca, String modelo, int ano) {
+		setMarca(marca);
+		setModelo(modelo);
+		setAno(ano);
+	}
+
+	public Automovel() {
+	}
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public String getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+
 	public int getAno() {
 		return ano;
 	}
+
 	public void setAno(int ano) {
 		this.ano = ano;
 	}
-	
+
 	public String getMensagem() {
 		if (ano > 2009) {
 			return "Carro Novo";
 		}
-		return "Carro velho";		
+		return "Carro velho";
 	}
-	
+
+	public List<Automovel> getListaVeiculos() {
+		List<Automovel> lista = new ArrayList<Automovel>();
+		lista.add(new Automovel("Ford", "Fiesta", 2012));
+		lista.add(new Automovel("Peugeot", "208", 2014));
+		lista.add(new Automovel("GM", "Corsa", 2009));
+		
+		return lista;
+	}
+
 }
