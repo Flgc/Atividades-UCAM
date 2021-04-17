f7ec5f081d96a505d5def773dd61f8120d5038fa
Fabio Luis <ml@fabiotecnico1.biz>
2021-04-16 21:30:23 -0300

    Implementando primeiro projeto Android - Sorteio Aula 2 - Codificando componente

--- AndroidStudioProjects/app/src/main/java/biz/fabiotecnico1/sorteio/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/biz/fabiotecnico1/sorteio/MainActivity.java
@@ -3,6 +3,8 @@ package biz.fabiotecnico1.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarBotao (View v){
+        TextView texto = findViewById(R.id.textoResultado);
+        texto.setText("TEXTO ALTERADO COM SUCESSO");
     }
 }
