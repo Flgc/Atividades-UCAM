cfe85ceb7aafb0d467709e45073d9ff80109e63f
Fabio Luis <ml@fabiotecnico1.biz>
2021-04-16 23:23:33 -0300

    Implementando Segundo projeto Android - App Calcula Alcool ou Gasolina - Aula 1 - Criando calculo e acção dos componentes - Final

--- AlcoolouGasolina/app/src/main/java/biz/fabiotecnico1/alcoolougasolina/MainActivity.java
+++ AlcoolouGasolina/app/src/main/java/biz/fabiotecnico1/alcoolougasolina/MainActivity.java
@@ -3,12 +3,43 @@ package biz.fabiotecnico1.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editPrecoAlcool;
+    private EditText editPrecoGasolina;
+    private TextView textResultado;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editAlcool);
+        editPrecoGasolina = findViewById(R.id.editGasolina);
+        textResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calcularPreco(View view){
+        //Convertendo o valor de string para double
+        Double precoAlcool = Double.parseDouble(editPrecoAlcool.getText().toString());
+        Double precoGasolina = Double.parseDouble(editPrecoGasolina.getText().toString());
+
+        /* Calcula (Preco Alcool / Preco Gasolina)
+            Se resultado for >= 0,7 melhor utilizar gasolina senão melhor utilizar alcool
+         */
+        Double resultado = precoAlcool/precoGasolina;
+
+        if(resultado >= 0.7){
+            textResultado.setText("Melhor abastecer com gasolina.");
+        } else {
+            textResultado.setText("Melhor abastecer com alcool.");
+        }
     }
 }
