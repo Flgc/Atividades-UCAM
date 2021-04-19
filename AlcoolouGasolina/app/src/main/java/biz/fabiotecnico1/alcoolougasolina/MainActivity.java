ba631f7af1bbccde8025144f53193b5db3cba1b2
Fabio Luis <ml@fabiotecnico1.biz>
2021-04-19 18:23:49 -0300

    Refaturando Segundo projeto Android - App Calcula Alcool ou Gasolina - Impedindo o calculo sem valores

--- AlcoolouGasolina/app/src/main/java/biz/fabiotecnico1/alcoolougasolina/MainActivity.java
+++ AlcoolouGasolina/app/src/main/java/biz/fabiotecnico1/alcoolougasolina/MainActivity.java
@@ -6,15 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editPrecoAlcool;
     private EditText editPrecoGasolina;
     private TextView textResultado;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +25,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcularPreco(View view){
-        //Convertendo o valor de string para double
-        Double precoAlcool = Double.parseDouble(editPrecoAlcool.getText().toString());
-        Double precoGasolina = Double.parseDouble(editPrecoGasolina.getText().toString());
 
-        /* Calcula (Preco Alcool / Preco Gasolina)
-            Se resultado for >= 0,7 melhor utilizar gasolina senão melhor utilizar alcool
-         */
-        Double resultado = precoAlcool/precoGasolina;
+        if (editPrecoAlcool.getText().toString().trim().equals("") ||
+                editPrecoGasolina.getText().toString().trim().equals("")){
 
-        if(resultado >= 0.7){
-            textResultado.setText("Melhor abastecer com gasolina.");
-        } else {
-            textResultado.setText("Melhor abastecer com alcool.");
+            Toast.makeText(this, "Valores incorretos",Toast.LENGTH_LONG).show();
+
+        }else{
+            //Convertendo o valor de string para double
+            Double precoAlcool = Double.parseDouble(editPrecoAlcool.getText().toString());
+            Double precoGasolina = Double.parseDouble(editPrecoGasolina.getText().toString());
+
+            /* Calcula (Preco Alcool / Preco Gasolina)
+               Se resultado for >= 0,7 melhor utilizar gasolina senão melhor utilizar alcool
+            */
+
+            Double resultado = precoAlcool/precoGasolina;
+
+            if(resultado >= 0.7){
+                textResultado.setText("Melhor abastecer com gasolina.");
+            } else {
+                textResultado.setText("Melhor abastecer com alcool.");
+            }
         }
     }
 }
