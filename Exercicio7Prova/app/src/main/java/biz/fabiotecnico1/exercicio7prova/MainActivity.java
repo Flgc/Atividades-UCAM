795efbe055fcd123a91b81cce61b0b44ff36dfb4
Fabio Luis <ml@fabiotecnico1.biz>
2021-04-19 17:56:25 -0300

    Desenvolvimento Android - Prova7 Layout

---
+++ Exercicio7Prova/app/src/main/java/biz/fabiotecnico1/exercicio7prova/MainActivity.java
@@ -0,0 +1,13 @@
+package biz.fabiotecnico1.exercicio7prova;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
